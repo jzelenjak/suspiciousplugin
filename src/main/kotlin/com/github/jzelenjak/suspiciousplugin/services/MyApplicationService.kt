39b59eea8e8bb61a7274a2ecd784f5cc114229b9
GitHub Action <action@github.com>
2022-04-24 18:32:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jzelenjak/suspiciousplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jzelenjak.suspiciousplugin.services
+
+import com.github.jzelenjak.suspiciousplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/jzelenjak/suspiciousplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jzelenjak/suspiciousplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jzelenjak.suspiciousplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jzelenjak.suspiciousplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
